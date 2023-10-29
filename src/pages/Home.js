72e54d217d400a4db53cb83d4bd56f3c320c87a8
DmPe91 <dmper67@gmail.com>
2023-10-29 18:34:25 +0300

    search problem

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,13 @@ import React, { useEffect, useContext } from "react";
 import { Context } from "..";
 import Shop from "../components/Shop";
 import OrdersBar from "../components/OrdersBar";
+import { observer } from "mobx-react-lite";
 
-export const Home = () => {
+export const Home = observer(() => {
   return (
     <>
       <Shop mt={3} />
       <OrdersBar />
     </>
   );
-};
+});
